bb04f219b245a62832d70631a2a2fa547254d3eb
vaishnavi <97829287+vaishnavighewaree@users.noreply.github.com>
2022-01-28 21:30:22 +0530

    Add files via upload

---
+++ practiceProject/Method.java
@@ -0,0 +1,28 @@
+package assignment.phase;
+
+public class Method {
+	public int multipynumbers(int a,int b) {
+		int z=a*b;
+		return z;
+	}
+
+	public static void main(String[] args) {
+
+		Method b=new Method();
+		int ans= b.multipynumbers(10,3);
+		System.out.println("Multipilcation is :"+ans);
+		}
+
+
+	
+	public class callMethod {
+
+	int val=150;
+
+	int operation(int val) {
+		val =val*10/100;
+		return(val);
+	}
+	}
+	}
+	
